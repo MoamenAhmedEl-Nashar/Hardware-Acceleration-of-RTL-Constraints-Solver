35b18366551cced087a53ae5bb7e7d5f4137a86e
Moamen Ahmed <moamen.ahmed1@hotmail.com>
2018-12-07 00:09:30 +0200

    propose function

--- MCMC_sampler.py
+++ MCMC_sampler.py
@@ -1,7 +1,10 @@
+import numpy as np
 import random
 import copy
+import math
+
 #################################
-random.seed(2)
+random.seed(1)
 
 
 
@@ -36,7 +39,7 @@ class IntegerLiteral:
     variable_names : [str]
     coefficient: [int]
 
-    def set_literal(self, no_of_int_variables: int,variable_names : [str], coefficient: [int]):
+    def set_literal(self, no_of_int_variables , variable_names , coefficient: [int]):
         self.no_of_int_variables = no_of_int_variables  # number of int_variables  [y1,y2,....,y3]
         self.variable_names =variable_names #y2,y1,y3,y5
         self.coefficient = coefficient  # coefficient is a set of integers
@@ -47,6 +50,8 @@ class IntegerLiteral:
         return self.coefficient
     def get_variable_names(self):
         return self.variable_names
+    def get_bias(self):
+        return self.coefficient[-1]
 
     def print_int_literal(self):
         for i in range(0, self.no_of_int_variables+1):
@@ -58,9 +63,11 @@ class IntegerLiteral:
         print(" <= 0 ")
 
     def reduce_literal(self, current_assignment: [int], variable_to_be_unchanged):
-        if variable_to_be_unchanged in self.variable_names:
-            reduced_integer_literal = IntegerLiteral()
-            reduced_integer_literal = copy.deepcopy(self)
+        reduced_integer_literal = IntegerLiteral()
+        reduced_integer_literal = copy.deepcopy(self)
+        if len(self.variable_names)==1 and variable_to_be_unchanged in self.variable_names:
+            return reduced_integer_literal
+        elif variable_to_be_unchanged in self.variable_names:
             new_bias = 0
             i: int
             for i in range(0, self.no_of_int_variables ):
@@ -178,13 +185,15 @@ class Sampler:
     def make_random_assignment_integer(self):
         n=self.formula.get_no_of_integer_variables()
         for i in range(0,n):
-            self.current_values_integer.append(random.randint(0,1000)) # assume 1000 is the maximum number
+            self.current_values_integer.append(random.randint(0,1000)) # assume 1000 is the maximum number 'make an enhancement' !
+            
         
 
     def make_random_assignment_boolean(self):
         n=self.formula.get_no_of_boolean_variables()
         for i in range(0,n):
-            self.current_values_boolean.append(random.randint(0,1)) 
+            self.current_values_boolean.append(random.randint(0,1))
+            
         
 
     #def set_values(self, values: [[int], [int]]):
@@ -203,45 +212,112 @@ class Sampler:
     def compute_temperature(self):
         pass
 
+
+
+    def propose (self,selected_integer_variable):
+        no_of_integer_variables=self.formula.get_no_of_integer_variables()
+        no_of_boolean_variables=self.formula.get_no_of_boolean_variables()
+        integer_variable_names=self.formula.get_integer_variable_names()
+        boolean_variable_names=self.formula.get_boolean_variable_names()
+        #get the proposed value of this selected integer varaible
+        clauses=self.formula.get_clauses()
+        first_bias=1000
+        second_bias=1001
+        for clause in clauses:
+            literals=clause.get_int_literals()
+            for literal in literals:
+                reduced_literal=literal.reduce_literal(self.current_values_integer,selected_integer_variable)
+                if reduced_literal != False:
+                    bias=reduced_literal.get_bias()
+                    if bias <= 0:#-ve bias
+                        first_bias=min(bias,first_bias)
+                    elif bias >0:#+ve bias
+                        second_bias=min(bias,second_bias)
+        #construct weights for probability distribution segments(3 segments)
+        c1=-first_bias   # y<c1  ,  y> c2
+        c2=second_bias
+        if c1>c2:#case 1  (exp1 uniform2 exp3)
+            w1=1/(1-math.exp(-1))  #1.58
+            w2=c1-c2+1
+            w3=1/(1-math.exp(-1))  #1.58
+            sum_w=w1+w2+w3
+            p1=w1/sum_w
+            p2=w2/sum_w
+            p3=w3/sum_w
+            #select segment according to the normalized propabilities p1,p2,p3
+            segments_numbers=[1,2,3]
+            probabilities=[p1,p2,p3]
+            selected_segment_number=np.random.choice(segments_numbers,p=probabilities)
+            #update current assignment with this new proposed value
+            if selected_segment_number==2:#uniform2
+                proposed_value=random.randint(c2,c1)
+            if selected_segment_number==1:#exp1
+                theta=random.uniform(0,w1)
+                d=math.ceil(-1-math.log(1-theta*(1-math.exp(-1))))
+                proposed_value=c2-d
+            if selected_segment_number==3:#exp3
+                theta=random.uniform(0,w3)
+                d=math.ceil(-1-math.log(1-theta*(1-math.exp(-1))))
+                proposed_value=c1+d
+        if c1<=c2:#case 2  (exp1 exp2)
+            mid=int((c1+c2)/2)
+            w1=1/(1-math.exp(-1))  #1.58
+            w2=1/(1-math.exp(-1))  #1.58
+            sum_w=w1+w2
+            p1=w1/sum_w
+            p2=w2/sum_w
+            #select segment according to the normalized propabilities p1,p2
+            segments_numbers=[1,2]
+            probabilities=[p1,p2]
+            selected_segment_number=np.random.choice(segments_numbers,p=probabilities)
+            #update current assignment with this new proposed value
+            if selected_segment_number==1:#exp1
+                theta=random.uniform(0,w1)
+                d=math.ceil(-1-math.log(1-theta*(1-math.exp(-1))))
+                proposed_value=mid-d
+            if selected_segment_number==2:#exp2
+                theta=random.uniform(0,w2)
+                d=math.ceil(-1-math.log(1-theta*(1-math.exp(-1))))
+                proposed_value=mid+d
+        
+        return proposed_value
+    
+
     def metropolis_move(self):
 
         no_of_integer_variables=self.formula.get_no_of_integer_variables()
         no_of_boolean_variables=self.formula.get_no_of_boolean_variables()
         integer_variable_names=self.formula.get_integer_variable_names()
         boolean_variable_names=self.formula.get_boolean_variable_names()
+        
         #select variable boolean or integer
-        random_index_is_int_or_bool=random.randint(0,1)    ## 1 --> int     0-->  boolean
-        if random_is_int_or_bool == 1:
-            v_index=randint(0,no_of_int_variables)
-        else :
-            v_index=randint(0,no_of_boolean_variables)
+        if len(boolean_variable_names)==0:
+            random_variable_is_int_or_bool=random.randint(1,1)
+        else:       
+            random_variable_is_int_or_bool=random.randint(0,1)    ## 1 --> int     0-->  boolean
+        if random_variable_is_int_or_bool == 0:
+            #select boolean variable 
+            selected_boolean_variable=random.choice(boolean_variable_names)
+            #flip the value of this selected boolean variable
+            self.current_values_boolean[boolean_variable_names.index(selected_boolean_variable)]=(self.current_values_boolean[boolean_variable_names.index(selected_boolean_variable)]-1)%2
+        else:
+            #select integer variable
+            selected_integer_variable=random.choice(integer_variable_names)
+            index_of_selected_integer_variable=integer_variable_names.index(selected_integer_variable)
+            proposed_value=self.propose(selected_integer_variable)
+            #update current integer assignment
+            self.current_values_integer[index_of_selected_integer_variable]=proposed_value
+            # Q calculating
 
-        if random_index_is_int_or_bool == 0 :  ## boolean variable
-            current_values[0][v_index]=(current_values[0][v_index]-1)%2  # flip
-        else : ## integer variable
-            current_values[1][v_index]=gibbs(self)
+            # U calculating
 
-    def gibbs (self,v_index):
-        pass
+            return self.current_values_integer
 
-    def project(self,v_index):
-        new_clause=clause()
-        old_clauses=formula.get_clauses() 
-        for old_clause in old_clauses:
-            old_int_literals=old_clause.get_int_literals()
-            for old_int_literal in old_int_literals:
-                pass
-        return new_clause
-        
-         
+    
 
-    def sample_from(new_clause):
-        pass
+
+    
             
-        
-            
-        
-
     def local_move(self): 
        pass
 
@@ -254,10 +330,10 @@ L1 = IntegerLiteral()
 L1.set_literal(2, integer_variable_names,[1, 1, -10])
 
 L2 = IntegerLiteral()
-L2.set_literal(1, integer_variable_names[0],[1, -10])
+L2.set_literal(1, ['y1'],[1, -10])
 
 L3 = IntegerLiteral()
-L3.set_literal(1, integer_variable_names[1],[1, -10])
+L3.set_literal(1, ['y2'],[1, -10])
 
 C1 = Clause()
 C1.set_clause(0, 3, [], [L1,L2,L3])
@@ -267,8 +343,17 @@ formula.set_formula(0 , 2 , [] ,integer_variable_names , 1, [C1])
 sampler=Sampler(formula,1,1)
 sampler.make_random_assignment_integer()
 sampler.make_random_assignment_boolean()
-#formula.print_formula()
 
+
+
+for i in range(0,99):
+    current=sampler.metropolis_move()
+    if current[0]<=10 and current[1]<=10 and current[0]+current[1]<=10:
+        print(current)
+        print('iteration:',i)
+        break
+#formula.print_formula()
+#print(L1.get_bias())
 #L1.print_int_literal()
 #new=L1.reduce_literal([11,11],'y1')
 #new.print_int_literal()
