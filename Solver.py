198431eefeb2517bb41ac87037ac6ff73151c0e5
youananNagy <youanan.nagy2@gmail.com>
2019-03-04 01:17:41 +0200

    the enhanced solver

--- Solver.py
+++ Solver.py
@@ -30,7 +30,11 @@ import math
 
 # boolean literal can be x^k or !x^k
 # boolean literal is not needed in this version of code
-
+#encoding
+UNIFORM=3
+EXP_UP=2
+EXP_DOWN=1
+#########
 class Clause:
     number_of_integer_literal = int
     boolean_literals = []
@@ -73,23 +77,10 @@ class Clause:
                 print("OR")
                 print("X", j)
 
-class Indicator:
-    _from = int
-    _to = int
-    _type = int
 
-    
-class Distribution:
-    indicators = [Indicator]
-
-#encoding
-UNIFORM=3
-EXP_UP=2
-EXP_DOWN=1
-#########
 
 class Solver:
-    clauses = []
+    formula = []
     current_values_int = []
     current_values_boolean = []
     number_of_clauses = int
@@ -97,18 +88,21 @@ class Solver:
     number_of_integer_variables = int
     pls0 = float
     pls = float
-    bit_width_for_int_variables = [32]*number_of_integer_variables # assume that all int variables is length 32 bit
+    temperature = float
+    bit_width_for_int_variables = [] # assume that all int variables is length 32 bit
 
 
-    def __init__(self, number_of_boolean_variables, number_of_integer_variables,number_of_clauses,pls0):
+    def __init__(self, number_of_boolean_variables, number_of_integer_variables,number_of_clauses,pls0,temperature=1):
         self.number_of_boolean_variables = number_of_boolean_variables
         self.number_of_integer_variables = number_of_integer_variables
         self.number_of_clauses = number_of_clauses
-        self.clauses = [Clause(number_of_boolean_variables, number_of_integer_variables) for i in range(0,number_of_clauses)]
+        self.formula = [Clause(number_of_boolean_variables, number_of_integer_variables) for i in range(0,number_of_clauses)]
         self.pls0 = pls0
+        self.temperature=temperature
+        self.bit_width_for_int_variables=[32] * number_of_integer_variables# assume that all int variables is length 32 bit
 
     def check_bool_literal(self,clause_num, bool_literal_num):
-        clause = self.clauses[clause_num]
+        clause = self.formula[clause_num]
         bool_literal = clause.boolean_literals[bool_literal_num]
 
         # if not exist
@@ -122,7 +116,7 @@ class Solver:
 
     def check_int_literal(self, clause_num, int_literal_num):
 
-        clause = self.clauses[clause_num]
+        clause = self.formula[clause_num]
         int_literal = clause.integer_literals[int_literal_num]
         count = 0
         for j in range(self.number_of_integer_variables):
@@ -134,7 +128,7 @@ class Solver:
         return False
 
     def check_clause(self,clause_num):  # from 0 to NUM_OF_CLAUSES-1
-        clause = self.clauses[clause_num]
+        clause = self.formula[clause_num]
 
 
         for j in range(clause.number_of_boolean_variables):
@@ -151,6 +145,16 @@ class Solver:
             if self.check_clause(i) == False:
                 return False
         return True
+
+    def find_number_of_unsatisfied_clauses(self,assignme_to_check_on_it):
+        buffer=self.current_values_int
+        self.current_values_int=assignme_to_check_on_it
+        count=0
+        for i in range(self.number_of_clauses):
+           if self.check_clause(i) == False:
+               count=count+1
+        self.current_values_int=buffer
+        return count
 
     def compute_pls(self,iteration_number):
         self.pls = self.pls0 * math.exp(1 - iteration_number)
@@ -169,10 +173,9 @@ class Solver:
         for i in range(self.number_of_boolean_variables):
             self.current_values_boolean.append(random.randint(0, 1))
     
-
     def reduce_literal(self, clause_num, int_literal_num, index_variable_to_be_unchanged):  # from 0 to NUM_OF_INT_VARIABLES-1
         #self.current_values_int =[1, 1, 1]
-        clause = self.clauses[clause_num]
+        clause = self.formula[clause_num]
         int_literal = clause.integer_literals[int_literal_num]
         reduced_int_literal = int_literal
 
@@ -206,8 +209,7 @@ class Solver:
         # should be something like the form of y1 + 10 <= 0 or -1 y1 +5 <= 0
         return reduced_int_literal
 
-    def get_active_clauses(self, index_variable_to_be_unchanged):  # return active formula
-
+    def construct_active_formula(self, index_variable_to_be_unchanged):  # return reduced active formula 
         active_formula =[]
         for i in range(self.number_of_clauses):
             skip = False
@@ -218,71 +220,189 @@ class Solver:
                     skip = 1  # the clause is satisfied
 
             if skip == 0:  # the clause is not satisfied
-                for k in range(self.clauses[i].number_of_integer_literal):
+                for k in range(self.formula[i].number_of_integer_literal):
                     reduced_int_literal = self.reduce_literal(i, k, index_variable_to_be_unchanged)
                     if reduced_int_literal == False:  # this variable is not in that int literal
                         if self.check_int_literal(i, k) == True:
                             skip = 1
 
             if skip == 0:  # the clause is not satisfied
-                for k in range(self.clauses[i].number_of_integer_literal):
+                for k in range(self.formula[i].number_of_integer_literal):
                     reduced_int_literal = self.reduce_literal(i, k, index_variable_to_be_unchanged)
                     if reduced_int_literal != False:  # this variable is in that int literal
                         active_clause.add_integer_literal(reduced_int_literal)
+
             active_formula.append(active_clause)
+
         return active_formula
 
-    def get_indicator(self,reduced_int_literal,index_variable_to_be_unchanged): #[1 0 0 5] or [0 -1 0 3]
+    def propose_value(self,reduced_active_formula,index_variable_to_be_unchanged):
+         #assuming that there is only one integer Literal in any clause
         
-        maximum=math.pow(2,self.bit_width_for_int_variables[index_variable_to_be_unchanged]-1)-1
-        minimum=-math.pow(2,self.bit_width_for_int_variables[index_variable_to_be_unchanged]-1)-1
+        case1_biases=[] # case one -> y + b < 0
+        case2_biases=[] # case two -> -y + b <0
+        for clause in reduced_active_formula:
+            if clause.integer_literals[0][index_variable_to_be_unchanged] > 0: # clause.integer_literals[0][index_variable_to_be_unchanged] -> returns the coeff. of the the chosed variable to be sambled
+                
+                case1_biases.append(clause.integer_literals[0][-1])
+            
+            elif clause.integer_literals[0][index_variable_to_be_unchanged] < 0:
 
-        indicator= Indicator()       
-        distribution = Distribution() 
-        if reduced_int_literal[index_variable_to_be_unchanged]==1:
-            # minimum --> -bias
-            indicator._from = minimum
-            indicator._to = -reduced_int_literal[-1]   
-            indicator._type = UNIFORM
-            distribution.indicators.append(indicator)
-            # -bias --> maximum
-            indicator._from = -reduced_int_literal[-1]
-            indicator._to = maximum 
-            indicator._type = EXP_DOWN
-            distribution.indicators.append(indicator)
+                case2_biases.append(clause.integer_literals[0][-1])
 
-        if reduced_int_literal[index_variable_to_be_unchanged]==-1:
-            # minimum --> bias
-            indicator._from = minimum
-            indicator._to = reduced_int_literal[-1] 
-            indicator._type = EXP_UP
-            distribution.indicators.append(indicator)
-            # bias --> maximum
-            indicator._from = reduced_int_literal[-1]
-            indicator._to = maximum
-            indicator._type = UNIFORM
-            distribution.indicators.append(indicator)
+        c1=-1*(min(case1_biases))
+        c2=min(case2_biases)
+        ## From the old code "sampler.py"
+        if c1 > c2:  # case 1  (exp1 uniform2 exp3)
+            w1 = 1 / (1 - math.exp(-1))  # 1.58
+            w2 = c1 - c2 + 1
+            w3 = 1 / (1 - math.exp(-1))  # 1.58
+            sum_w = w1 + w2 + w3
+            p1 = w1 / sum_w
+            p2 = w2 / sum_w
+            p3 = w3 / sum_w
+            # select segment according to the normalized propabilities p1,p2,p3
+            segments_numbers = [1, 2, 3]
+            probabilities = [p1, p2, p3]
+            selected_segment_number = np.random.choice(segments_numbers, p=probabilities)
+            # update current assignment with this new proposed value
+            if selected_segment_number == 2:  # uniform2
+                proposed_value = random.randint(c2, c1)
+            if selected_segment_number == 1:  # exp1
+                theta = random.uniform(0, w1)
+                d = math.ceil(-1 - math.log(1 - theta * (1 - math.exp(-1))))
+                proposed_value = c2 - d
+            if selected_segment_number == 3:  # exp3
+                theta = random.uniform(0, w3)
+                d = math.ceil(-1 - math.log(1 - theta * (1 - math.exp(-1))))
+                proposed_value = c1 + d
+        if c1 <= c2:  # case 2  (exp1 exp2)
+            mid = int((c1 + c2) / 2)
+            w1 = 1 / (1 - math.exp(-1))  # 1.58
+            w2 = 1 / (1 - math.exp(-1))  # 1.58
+            sum_w = w1 + w2
+            p1 = w1 / sum_w
+            p2 = w2 / sum_w
+            # select segment according to the normalized propabilities p1,p2
+            segments_numbers = [1, 2]
+            probabilities = [p1, p2]
+            selected_segment_number = np.random.choice(segments_numbers, p=probabilities)
+            # update current assignment with this new proposed value
+            if selected_segment_number == 1:  # exp1
+                theta = random.uniform(0, w1)
+                d = math.ceil(-1 - math.log(1 - theta * (1 - math.exp(-1))))
+                proposed_value = mid - d
+            if selected_segment_number == 2:  # exp2
+                theta = random.uniform(0, w2)
+                d = math.ceil(-1 - math.log(1 - theta * (1 - math.exp(-1))))
+                proposed_value = mid + d
 
-        return distribution
+        #need to add case 3 for the OR case
+        return proposed_value
 
-    def get_segments_from_active_clause(self,active_clause,index_variable_to_be_unchanged):
-        case1=[]
-        case2=[]
-        for literal in active_clause.integer_literals:
-                      
-            if literal[index_variable_to_be_unchanged] > 0:
-                case1.append(get_indicator(int_literal))
-            else:
-                case2.append(get_indicator(int_literal)
+    def flib(self,index_variable_to_be_changed):
+
+        if self.current_values_boolean[index_variable_to_be_changed]==1:
+            self.current_values_boolean[index_variable_to_be_changed]=0
+        else:
+            self.current_values_boolean[index_variable_to_be_changed]=1
+
+    def pick_variable_to_be_sampled(self):
+        x=random.randint(0,1)
+        _type=""
+        chosen_var=0
+        if x == 1 :
+            _type="bool"
+            chosen_var = random.choice(range(self.number_of_boolean_variables))
+        else:
+            _type="integer"
+            chosen_var = random.choice(range(self.number_of_integer_variables))
+        return chosen_var,_type
+    def calculate_proposed_distribution(self):
+        # we shoud here calculate Q ˜ p(x,y) in the thesis page 18
+        # but right now we hard coded just to 0.5
+        Q=0.5
+        return Q
+    
+    def metropolis_move(self):
+        chosen_var,_type = self.pick_variable_to_be_sampled()
+
+        if _type =="bool":
+            self.flib(chosen_var)
+        else: #_type == "integer"
+
+            unsatisfied_clauses_before_the_move=self.find_number_of_unsatisfied_clauses(self.current_values_int)
+
+            active_formula=self.construct_active_formula(chosen_var)
+            proposed_value=self.propose_value(active_formula,chosen_var)
+
+            # Update the proposed move to be equals the old assignment except the chosen_variable 
+            proposed_move = self.current_values_int
+            proposed_move[chosen_var]=proposed_value
+            
+            proposed_distribution = self.calculate_proposed_distribution() # proposed_distribution = 'Q' in the thises
+
+            unsatisfied_clauses_after_the_move=self.find_number_of_unsatisfied_clauses(proposed_move)
+
+            #calculate target distribution = Q*e^(U`-U) with the notation of the thesis 
+            target_distribution=proposed_distribution * (math.exp(-((unsatisfied_clauses_after_the_move - unsatisfied_clauses_before_the_move) / self.temperature)))
+            pr_do_change = min(1,target_distribution )
+            pr_stay = 1 - pr_do_change
+
+            choice = np.random.choice(['do_change', 'stay'], p=[pr_do_change, pr_stay])
+
+            if choice == 'do_change':
+               self.current_values_int = proposed_move
+            
+            return
+
+    def Local_search_move(self):
+        # dont return a value just update the current assimnt if needed, just like the metropolis 
+        pass
+
+
+    def solve(self):
+        #make random assignments
+        self.make_random_assignment_int()
+        print('random',self.current_values_int)
+        self.make_random_assignment_bool()
+        #metropolis
+        self.metropolis_move()
+        counter=1
+        print(counter,self.current_values_int)
+
+        while self.check_formula()==False: #problem here
+            counter+=1
+            self.compute_pls(counter)
+            choice=np.random.choice(['local','metropolis'], p=[self.pls,1-self.pls])
+
+            if choice == 'local':
+                print("local")
+                self.local_move()
+
+            elif choice == 'metropolis':
+                print("metropolis")
+                self.metropolis_move()
+            print(counter,self.current_values_int)
+
+
+
+
+
+
+
+
+
+
 
 myClause = Clause(2,3)
 myClause.add_integer_literal([1,1,1,4])
 myClause.add_integer_literal([11,22,33,44])
 myClause.add_boolean_literal(0,0)
 #myClause.print_clause()
-'''
+
 mySolver = Solver()
-mySolver.clauses.append(myClause)
+mySolver.formula.append(myClause)
 
 myClause1 = Clause(2,3)
 myClause1.add_integer_literal([10,20,30,40])
@@ -290,8 +410,8 @@ myClause1.add_integer_literal([100,200,300,400])
 myClause1.add_boolean_literal(0,0)
 #myClause1.print_clause()
 
-mySolver.clauses.append(myClause1)
+mySolver.formula.append(myClause1)
 
-mySolver.clauses[0].print_clause()
+mySolver.formula[0].print_clause()
 
-print(mySolver.reduce_literal(0,0,1))'''
+print(mySolver.reduce_literal(0,0,1))
